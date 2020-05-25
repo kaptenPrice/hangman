8703bc80f70a3aff8a86cb802cb29e486330fe9f
81khameh <khazar.mehraban@gmail.com>
2020-05-25 15:33:29 +0200

    second commit, includs bg.img and button

--- hangingman/src/App.js
+++ hangingman/src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import hanger from './images/hanger.jpg';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+  
+       <h2>**"Hänga gubbe"**</h2>
+       <p> Hänga gubbe är ett spel. Spelet går ut på att gissa bokstäver i ett ord vars bokstäver
+      initialt är helt dolda,<br></br> men som visas som ledtrådar när spelaren lyckats gissa på dem.</p>
+    <img src={hanger} className="hanger"/>
+    <h2>- - - - - -</h2>
+    <p>**"Antal gissningar: 0"**</p>
+<button id="button">a</button>
+
+
+       
+   
     </div>
   );
 }
