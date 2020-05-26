6d8fc346fcb2ace4c26b5f7d2527b3619c3c3d2b
81khameh <khazar.mehraban@gmail.com>
2020-05-26 19:39:18 +0200

    button change color onclick

--- hangingman/src/App.js
+++ hangingman/src/App.js
@@ -2,27 +2,48 @@ import React from 'react';
 import './App.css';
 import hanger from './images/hanger.jpg';
 
+const buttonStyle={
+  background:"#bdbdbd",
+  boxShadow:"0px 3px rgba(63, 63, 63, 0.521)",
+  color:"white"
 
-  let alphabet="abcdefghijklmnopqrstuvwxyzåäö";
-  let keyboard= alphabet.split("").map(element=>(<button>{element}</button>));
 
+}
+var keyPressed=(e)=>{
+document.getElementById(e).style.boxShadow=buttonStyle.boxShadow;  
+document.getElementById(e).style.background=buttonStyle.background;  
+document.getElementById(e).style.color=buttonStyle.color;  
+
+//document.getElementById(e).id="pressed";
+//console.log(document.getElementById(e).style);
+}
+
+
+const alphabet = "abcdefghijklmnopqrstuvwxyzåäö";
+const keyboard = alphabet.split('').map( letter =>(
+     <button id={letter} key={letter} onClick={()=>keyPressed(letter)}>
+     {letter}
+     </button>
+   )
+   );
 
 function App() {
   return (
     <div className="container">
-  
-       <h2>**"Hänga gubbe"**</h2>
-       <p> Hänga gubbe är ett spel. Spelet går ut på att gissa bokstäver i ett ord vars bokstäver
-      initialt är helt dolda,<br></br> men som visas som ledtrådar när spelaren lyckats gissa på dem.</p>
-    <img src={hanger} className="hanger"/>
-    <h2>- - - - - -</h2>
-    <p>**"Antal gissningar: 0"**</p>
-    <div className="buttonBox">
-    {keyboard}
-    </div>
 
-       
-   
+      <h2>**"Hänga gubbe"**</h2>
+      <p>Spelets  regler: spelet går ut på att gissa bokstäver i ett ord vars bokstäver
+      är helt dolda,<br></br> men som visas som ledtrådar när spelaren lyckats gissa på dem.</p>
+      <img src={hanger} className="hanger" alt="hanger" />
+      <h2>- - - - - -</h2>
+      <p>**"Antal gissningar: 0"**</p>
+      <button id="test">Test</button>
+      <div className="buttonBox">
+        {keyboard} 
+      </div>
+
+
+
     </div>
   );
 }
